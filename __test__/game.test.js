db0d05466badc6185cff727a1a92f8a7e857161a
EntityFX <artem.solopiy@gmail.com>
2021-03-10 20:26:33 +0300

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -22,7 +22,7 @@ describe('Game', () => {
   describe('Game', () => {
     it('Initializes with two players', async () => {
       expect(game.p1).toBe('Salem')
-      expect(game.p2).toBe('Bananas')
+      expect(game.p2).toBe('Nate')
     })
 
     it('Initializes with an empty board', async () => {
@@ -60,7 +60,7 @@ describe('Game', () => {
       const game = new Game(p1, p2)
       expect(game.player).toBe('Salem')
       game.nextPlayer()
-      expect(game.player).toBe('Nate')
+      expect(game.player).toBe('Bananas')
     })
   })
 
